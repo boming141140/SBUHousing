8964f64b81c46c88f71a8f4e2193d2101746248a
boming141140 <1269847618zzh@gmail.com>
2020-12-27 02:03:51 -0500

    first commit

---
+++ script/main.js
@@ -0,0 +1,13 @@
+const scene= new THREE.Scene();
+const camera= new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1,1000);
+hlight = new THREE.AmbientLight(0x404040, 100);
+scene.add(hlight);
+const render = new THREE.WebGLRenderer();
+render.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(render.domElement);
+let loader = new THREE.GLTFLoader();
+loader.load('../Model/myHome.gltf',function(gltf){
+	scene.add(gltf.scene);
+	renderer.render(scene,camera);
+});
+
