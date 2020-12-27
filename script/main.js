22fb3d9437b32915e633aad4f69889c852410d7f
boming141140 <1269847618zzh@gmail.com>
2020-12-27 04:46:41 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -3,6 +3,9 @@ const camera= new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHe
 camera.position.x=0;
 camera.position.y=0;
 camera.position.z=20;
+
+controls = new THREE.OrbitControls(camera);
+controls.addEventListener('change',renderer);
 hlight = new THREE.AmbientLight(0x404040, 100);
 scene.add(hlight);
 const renderer = new THREE.WebGLRenderer();
@@ -11,8 +14,13 @@ document.body.appendChild(renderer.domElement);
 let loader = new THREE.GLTFLoader();
 loader.load('Model/scene.gltf',function(gltf){
 	scene.add(gltf.scene);
-	renderer.render(scene,camera);
+	animate();
 });
+function animate(){
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+}
+
 
 
 
