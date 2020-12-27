f2e19d75a58a93d661e70884d9db74c57db2e8fc
boming141140 <69752753+boming141140@users.noreply.github.com>
2020-12-27 04:06:49 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ const camera= new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHe
 hlight = new THREE.AmbientLight(0x404040, 100);
 scene.add(hlight);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+render.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 let loader = new THREE.GLTFLoader();
 loader.load('Model/scene.gltf',function(gltf){
